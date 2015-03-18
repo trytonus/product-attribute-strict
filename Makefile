37225e40d065fd54fd9f629699bbed75e085c415
Priyanka Rani <priyanka.rani@openlabs.co.in>
2015-03-18 17:32:41 -0500

    Allow products to have attributes #7147

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ test: test-sqlite test-postgres test-flake8
 
 test-sqlite: install-dependencies
 	coverage run setup.py test
-	coverage report -m --fail-under 79
+	coverage report -m --fail-under 100
 
 test-postgres: install-dependencies
 	python setup.py test_on_postgres
--- __init__.py
+++ __init__.py
@@ -7,8 +7,20 @@
 """
 from trytond.pool import Pool
 
+from product import (
+    ProductAttributeSet, ProductAttributeSelectionOption, ProductAttribute,
+    ProductAttributeAttributeSet, Template, ProductProductAttribute, Product
+)
+
 
 def register():
     Pool.register(
+        ProductAttributeSet,
+        ProductAttribute,
+        ProductAttributeSelectionOption,
+        ProductAttributeAttributeSet,
+        Template,
+        ProductProductAttribute,
+        Product,
         module='product_attribute_strict', type_='model'
     )
