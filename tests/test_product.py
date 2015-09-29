1f0f14ea6adf7de716dce4dc695169f0512f49e5
Priyanka Rani <priyanka.rani@fulfil.io>
2015-09-28 23:57:28 -0500

    Improve test coverage

--- tests/test_product.py
+++ tests/test_product.py
@@ -207,6 +207,22 @@ class TestProduct(unittest.TestCase):
                     ]
                 }])
 
+            # check on_change value for attribute type
+            prod_attribute = prod1.attributes[0]
+            prod_attribute.on_change_attribute()
+
+            self.assertEqual(
+                prod_attribute.attribute_type, prod_attribute.attribute.type_
+            )
+
+            # check value for attribute set by changing product
+            prod_attribute.on_change_product()
+
+            self.assertEqual(
+                prod_attribute.attribute_set,
+                prod_attribute.product.template.attribute_set
+            )
+
 
 def suite():
     """
